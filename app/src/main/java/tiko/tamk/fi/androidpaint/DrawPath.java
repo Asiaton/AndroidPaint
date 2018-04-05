7d80316d0b259dbef0c02fd46ee2ebb41511a180
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-05 17:26:55 +0300

    Add classes DrawPath and PaintView
    -(DrawPath): Tracks finger movement on screen
    -(PaintView): View that paints given objects

---
+++ app/src/main/java/tiko/tamk/fi/androidpaint/DrawPath.java
@@ -0,0 +1,8 @@
+package tiko.tamk.fi.androidpaint;
+
+/**
+ * Created by TomB on 5.4.2018.
+ */
+
+public class DrawPath {
+}
---
+++ app/src/main/java/tiko/tamk/fi/androidpaint/PaintView.java
@@ -0,0 +1,10 @@
+package tiko.tamk.fi.androidpaint;
+
+import android.view.View;
+
+/**
+ * Created by TomB on 5.4.2018.
+ */
+
+public class PaintView extends View {
+}
