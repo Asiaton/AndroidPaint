97e78cb2fdbe7ad17561fec299b4ebc199049263
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-05 17:37:09 +0300

    Modify MainActivity add methods for menu creation
    -(onCreateOptionsMenu): Add new method
    -(onOptionsItemSelected): Add new method

--- app/src/main/java/tiko/tamk/fi/androidpaint/DrawPath.java
+++ app/src/main/java/tiko/tamk/fi/androidpaint/DrawPath.java
@@ -4,5 +4,61 @@ package tiko.tamk.fi.androidpaint;
  * Created by TomB on 5.4.2018.
  */
 
+import android.graphics.Path;
+
 public class DrawPath {
+
+    private int color;
+    private boolean emboss;
+    private boolean blur;
+    private int strokeWidth;
+    private Path path;
+
+    public DrawPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public boolean getEmboss() {
+        return emboss;
+    }
+
+    public void setEmboss(boolean emboss) {
+        this.emboss = emboss;
+    }
+
+    public boolean getBlur() {
+        return blur;
+    }
+
+    public void setBlur(boolean blur) {
+        this.blur = blur;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
 }
