4a216d4515508c13a2710ce9a73832336a276473
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-09 16:20:01 +0300

    Modify MainActivity.java add new methods
    -(save): Add new method for saving drawn
    image to device external storage
    -(createUniqueFileName): Add new method for
    assisting with saving images

--- app/src/main/java/tiko/tamk/fi/androidpaint/MainActivity.java
+++ app/src/main/java/tiko/tamk/fi/androidpaint/MainActivity.java
@@ -1,17 +1,33 @@
 package tiko.tamk.fi.androidpaint;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Calendar;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private PaintView paintView;
+    public final static String APP_PATH_SD_CARD = "/Paint05/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,24 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         paintView.init(displayMetrics);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            String [] listOfPermissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+            ActivityCompat.requestPermissions(this, listOfPermissions, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                          String permissions[],
+                                          int[] grantResults) {
+        if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        }
     }
 
     @Override
@@ -51,9 +85,75 @@ public class MainActivity extends AppCompatActivity {
             case R.id.clear:
                 paintView.clear();
                 return true;
+            case R.id.save:
+                save(paintView);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void save(PaintView paintView) {
+
+        Bitmap image = Bitmap.createBitmap(paintView.getWidth(),
+                paintView.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(image);
+        paintView.draw(c);
+
+
+        String fullPath =
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES).getAbsolutePath()
+                        + APP_PATH_SD_CARD;
+
+        System.out.println(fullPath);
+        try {
+            File dir = new File(fullPath);
+            System.out.println(dir.exists());
+            if (!dir.exists()) {
+                // TODO find out how to find phone gallery path and add here
+                System.out.println(dir.mkdirs());
+            }
+
+            OutputStream fOut = null;
+            Log.d("File", "1");
+            File file = new File(fullPath, createUniqueFileName() + ".png");
+
+            Log.d("File", "2");
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.createNewFile());
+
+            Log.d("File", "3");
+            fOut = new FileOutputStream(file);
+            Log.d("File", file.getAbsolutePath());
+
+            // 100 means no compression, the lower you go,
+            // the stronger the compression
+            image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+
+            MediaStore.Images.Media.insertImage(this.getContentResolver(),
+                    file.getAbsolutePath(), file.getName(), file.getName());
+
+        } catch (Exception e) {
+            Log.e("saveToExternalStorage()", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public String createUniqueFileName() {
+        String uniqueName = "img-";
+        Calendar c = Calendar.getInstance();
+        uniqueName += c.get(Calendar.YEAR);
+        uniqueName += String.format("%02d", c.get(Calendar.MONTH));
+        uniqueName += String.format("%02d", c.get(Calendar.DAY_OF_MONTH));
+        uniqueName += "-";
+        uniqueName += String.format("%02d", c.get(Calendar.HOUR_OF_DAY));
+        uniqueName += String.format("%02d", c.get(Calendar.MINUTE));
+        uniqueName += String.format("%02d", c.get(Calendar.SECOND));
+
+        return uniqueName;
     }
 
     public AmbilWarnaDialog createColorPicker() {
