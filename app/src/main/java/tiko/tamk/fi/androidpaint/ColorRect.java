c480c4180b6d6f99c1dadef6b0f1a2b6b38bf98d
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-23 01:07:53 +0300

    Modify ColorRect.java add attribute shape
    -(shape): Add enum to define what shape the
    rectangle is

--- app/src/main/java/tiko/tamk/fi/androidpaint/ColorRect.java
+++ app/src/main/java/tiko/tamk/fi/androidpaint/ColorRect.java
@@ -1,6 +1,5 @@
 package tiko.tamk.fi.androidpaint;
 
-import android.graphics.Color;
 import android.graphics.RectF;
 
 /**
@@ -10,14 +9,19 @@ import android.graphics.RectF;
 public class ColorRect extends ColorShape {
 
     private RectF rectangle;
+    private RectangleShape shape;
 
-    public ColorRect(int color, int strokeWidth, RectF rectangle) {
+    public ColorRect(int color, int strokeWidth, RectF rectangle, RectangleShape shape) {
         super(color, strokeWidth);
         this.rectangle = rectangle;
+        this.shape = shape;
     }
 
     public RectF getRectangle() {
         return rectangle;
     }
 
+    public RectangleShape getShape() {
+        return shape;
+    }
 }
