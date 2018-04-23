8b84176e800e304f86529391b5730cd80f5dd45b
Tom Bäckman <tom.backman@cs.tamk.fi>
2018-04-23 21:45:29 +0300

    Modify ColorShape.java add javadoc comments

--- app/src/main/java/tiko/tamk/fi/androidpaint/ColorShape.java
+++ app/src/main/java/tiko/tamk/fi/androidpaint/ColorShape.java
@@ -1,23 +1,42 @@
 package tiko.tamk.fi.androidpaint;
 
+/**
+ * Super class used to pass its methods and attributes to subclasses,
+ * and also to create a List that can contain all kinds of shapes.
+ */
 public class ColorShape {
 
+    /**
+     * The color in which the shape was drawn.
+     */
     private int color;
+
+    /**
+     * The width of the line with which the shape was drawn.
+     */
     private int strokeWidth;
 
+    /**
+     * Constructs the object and sets its attributes.
+     *
+     * @param color The color in which the shape was drawn.
+     * @param strokeWidth The width of the line with which the shape was drawn.
+     */
     public ColorShape(int color, int strokeWidth) {
         this.color = color;
         this.strokeWidth = strokeWidth;
     }
 
+    /**
+     * @return The color in which the shape was drawn.
+     */
     public int getColor() {
         return color;
     }
 
-    public void setColor(int color) {
-        this.color = color;
-    }
-
+    /**
+     * @return The width of the line with which the shape was drawn.
+     */
     public int getStrokeWidth() {
         return strokeWidth;
     }
